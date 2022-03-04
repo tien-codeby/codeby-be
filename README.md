67bdd36f24a60a2af0d388e99894e8546370e5cf
tien.luckyfor <tien.luckyfor@gmail.com>
2022-03-04 10:57:55 +0700

    init project

---
+++ README.md
@@ -0,0 +1,24 @@
+# setup project
+## env
+```
+cp .env.example .env;
+composer install;
+php artisan migrate:refresh; php artisan passport:install; 
+copy Client secret to .env file
+```
+## nginx
+```
+copy file ./nginx.conf 
+to laradock at
+path: nginx/sites/codeby-be.conf
+```
+## postman
+```
+MAIN.postman_collection.json
+```
+
+# cache 
+php artisan lighthouse:clear-cache
+
+[//]: # (# copy)
+[//]: # (cp -fR /Users/nguyenvantien/Jobs/boilerplate/laravel-graphql/* /Users/nguyenvantien/Jobs/codeby/codeby-be)
---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use Laravel\Passport\Passport;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+        Passport::routes();
+        ResetPassword::createUrlUsing(function ($user, string $token) {
+            return 'https://example.com/reset-password?token='.$token;  // Here is your custom url
+        });
+        //
+    }
+}
