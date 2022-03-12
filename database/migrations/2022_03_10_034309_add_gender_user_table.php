d013c06c1632de7ba9536d143fa83cd86f2bb7b9
dvTinh99 <tinh5969@gmail.com>
2022-03-12 19:40:59 +0700

    add email null

--- database/migrations/2022_03_10_034309_add_gender_user_table.php
+++ database/migrations/2022_03_10_034309_add_gender_user_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class AddGenderUserTable extends Migration
@@ -15,7 +16,7 @@ class AddGenderUserTable extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->boolean('gender')->nullable();
-            $table->string('email')->nullable()->change();
+            DB::statement('ALTER TABLE users MODIFY COLUMN email varchar(255) NULL');
         });
     }
 
